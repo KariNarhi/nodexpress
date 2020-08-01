551b7959e8b4e4d4b93b34a204ef230838f580ea
Kari Narhi <Kari.Narhi24@hotmail.com>
2020-08-01 19:10:20 +0300

    Code cleaning

--- app.js
+++ app.js
@@ -104,81 +104,8 @@ app.get("/", (req, res) => {
   });
 });
 
-// Get single article
-app.get("/article/:id", (req, res) => {
-  Article.findById(req.params.id, (err, article) => {
-    res.render("article", {
-      article: article,
-    });
-  });
-});
-
-// Add route
-app.get("/articles/add", (req, res) => {
-  res.render("add_article", {
-    title: "Add Article",
-  });
-});
-
-// Add Submit POST route
-app.post("/articles/add", (req, res) => {
-  let article = new Article();
-  article.title = req.body.title;
-  article.author = req.body.author;
-  article.body = req.body.body;
-
-  article.save((err) => {
-    if (err) {
-      console.log(err);
-      return;
-    } else {
-      req.flash("success", "Article added");
-      res.redirect("/");
-    }
-  });
-});
-
-// Load edit form
-app.get("/article/edit/:id", (req, res) => {
-  Article.findById(req.params.id, (err, article) => {
-    res.render("edit_article", {
-      title: "Edit article",
-      article: article,
-    });
-  });
-});
-
-// Update Submit POST route
-app.post("/article/edit/:id", (req, res) => {
-  let article = {};
-  article.title = req.body.title;
-  article.author = req.body.author;
-  article.body = req.body.body;
-
-  let query = { _id: req.params.id };
-
-  Article.update(query, article, (err) => {
-    if (err) {
-      console.log(err);
-      return;
-    } else {
-      req.flash("success", "Article updated");
-      res.redirect("/");
-    }
-  });
-});
-
-// DELETE route
-app.delete("/article/:id", function (req, res) {
-  let query = { _id: req.params.id };
-
-  Article.deleteOne(query, function (err) {
-    if (err) {
-      console.log(err);
-    }
-    res.send("Success");
-  });
-});
+let articles = require("./routes/articles.js");
+app.use("/articles", articles);
 
 // Start server
 app.listen(3000, () => {
