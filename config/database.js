1d7ea6d99c479b5b3602179ac8b64364e24a1235
Kari Narhi <Kari.Narhi24@hotmail.com>
2020-08-04 19:44:56 +0300

    Added database config

--- config/database.js
+++ config/database.js
@@ -1,4 +1,25 @@
-module.exports = {
-  database: "mongodb://localhost:27017/nodexpress",
-  secret: "nodex",
-};
+const mongoose = require("mongoose");
+
+const database = "mongodb://localhost:27017/nodexpress";
+
+// Database setup
+function db_setup() {
+  // Mongoose setup
+  mongoose.connect(database, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  let db = mongoose.connection;
+
+  // Check connection
+  db.once("open", () => {
+    console.log("Connected to MongoDB");
+  });
+
+  // Check for db errors
+  db.on("error", (err) => {
+    console.log(err);
+  });
+}
+
+module.exports = db_setup;
